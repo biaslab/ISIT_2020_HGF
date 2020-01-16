fb60e796954fccd5fa79770ff9f2ee710413e65e
Ivan Bocharov <bocharovia@gmail.com>
2020-01-16 20:11:05 +0100

    Restructured the packags. Added all the dependencies and imports.

--- src/GCVNode.jl
+++ src/GCVNode.jl
@@ -1,5 +1,20 @@
 module GCVNode
 
-greet() = print("Hello World!")
+using ForneyLab
+import ForneyLab: outboundType, isApplicable, generateId, unsafeMean, unsafeVar, @ensureVariables, addNode!, associate!, slug, format, FactorNode, SoftFactor, Interface, VariateType, unsafeMeanCov
+
+include("./helpers.jl")
+
+include("HGF/HGF.jl")
+include("nodes/exponential_linear_quadratic.jl")
+include("nodes/gaussian_controlled_variance.jl")
+
+include("rules/definition/equality.jl")
+include("rules/definition/gaussian_controlled_variance.jl")
+include("rules/definition/gaussian_mean_precision.jl")
+
+include("rules/implementation/equality.jl")
+include("rules/implementation/gaussian_controlled_variance.jl")
+include("rules/implementation/gaussian_mean_precision.jl")
 
 end # module
